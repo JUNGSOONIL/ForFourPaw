3b0b155c1811cc631a1d50b82c32074acda8db53
최상후 <csh1996kr@gmail.com>
2022-03-14 09:46:25 +0900

    Add backend basic

---
+++ backend/FFP/src/main/java/com/ssafy/FFP/FfpApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.FFP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfpApplication.class, args);
+	}
+
+}
