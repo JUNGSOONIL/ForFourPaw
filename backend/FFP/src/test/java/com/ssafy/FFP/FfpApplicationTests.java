d0bb03dbd7ca21d99628d9a847a8f75bed52f78e
최상후 <csh1996kr@gmail.com>
2022-03-14 09:46:25 +0900

    Add backend basic

---
+++ backend/FFP/src/test/java/com/ssafy/FFP/FfpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.FFP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FfpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
