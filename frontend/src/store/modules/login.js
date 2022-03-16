f5c6854f64be11b1e0b1046900377fe89a0bd6cb
윤찬호 <53756816+cmsj72@users.noreply.github.com>
2022-03-16 13:12:16 +0900

    Add Login Service

---
+++ frontend/src/store/modules/login.js
@@ -0,0 +1,24 @@
+const state = {
+  isLogin: false,
+};
+
+// getters
+const getters = {
+  isLogin: function (state) {
+    return state.isLogin;
+  },
+};
+
+// mutations
+const mutations = {};
+
+// actions
+const actions = {};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions,
+};
