c93be740e5a029012455378a55c1c9b5da86b49f
최상후 <csh1996kr@gmail.com>
2022-03-18 11:20:37 +0900

    save

--- backend/FFP/src/main/java/com/ssafy/FFP/Controller/UserController.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ssafy.FFP.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
