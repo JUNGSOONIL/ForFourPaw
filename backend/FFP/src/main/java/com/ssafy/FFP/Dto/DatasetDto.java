00402fc8db54dbc79bd3e136c5dc7eb234b3375c
soonil <soonil0119@naver.com>
2022-03-26 17:59:05 +0900

    Update DatasetDto.java add no MissMatchServiceImpl.java update happenPlace
    Former-commit-id: a01a2889cd5fd1c4e03f07cc8c98a9790bcb21f1

--- backend/FFP/src/main/java/com/ssafy/FFP/Dto/DatasetDto.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Dto/DatasetDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.FFP.Dto;
 
 public class DatasetDto {
+	private int no;
     private String desertionNo;
     private String filename;
     private String happenDt;
@@ -23,153 +24,26 @@ public class DatasetDto {
     private String orgNm;
     private String chargeNm;
     private String officetel;
-	public String getDesertionNo() {
-		return desertionNo;
-	}
-	public void setDesertionNo(String desertionNo) {
-		this.desertionNo = desertionNo;
-	}
-	public String getFilename() {
-		return filename;
-	}
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-	public String getHappenDt() {
-		return happenDt;
-	}
-	public void setHappenDt(String happenDt) {
-		this.happenDt = happenDt;
-	}
-	public String getHappenPlace() {
-		return happenPlace;
-	}
-	public void setHappenPlace(String happenPlace) {
-		this.happenPlace = happenPlace;
-	}
-	public String getKindCd() {
-		return kindCd;
-	}
-	public void setKindCd(String kindCd) {
-		this.kindCd = kindCd;
-	}
-	public String getColorCd() {
-		return colorCd;
-	}
-	public void setColorCd(String colorCd) {
-		this.colorCd = colorCd;
-	}
-	public String getAge() {
-		return age;
-	}
-	public void setAge(String age) {
-		this.age = age;
-	}
-	public String getWeight() {
-		return weight;
-	}
-	public void setWeight(String weight) {
-		this.weight = weight;
-	}
-	public String getNoticeNo() {
-		return noticeNo;
-	}
-	public void setNoticeNo(String noticeNo) {
-		this.noticeNo = noticeNo;
-	}
-	public String getNoticeSdt() {
-		return noticeSdt;
-	}
-	public void setNoticeSdt(String noticeSdt) {
-		this.noticeSdt = noticeSdt;
-	}
-	public String getNoticeEdt() {
-		return noticeEdt;
-	}
-	public void setNoticeEdt(String noticeEdt) {
-		this.noticeEdt = noticeEdt;
-	}
-	public String getPopfile() {
-		return popfile;
-	}
-	public void setPopfile(String popfile) {
-		this.popfile = popfile;
-	}
-	public String getProcessState() {
-		return processState;
-	}
-	public void setProcessState(String processState) {
-		this.processState = processState;
-	}
-	public String getSexCd() {
-		return sexCd;
-	}
-	public void setSexCd(String sexCd) {
-		this.sexCd = sexCd;
-	}
-	public String getNeuterYn() {
-		return neuterYn;
-	}
-	public void setNeuterYn(String neuterYn) {
-		this.neuterYn = neuterYn;
-	}
-	public String getSpecialMark() {
-		return specialMark;
-	}
-	public void setSpecialMark(String specialMark) {
-		this.specialMark = specialMark;
-	}
-	public String getCareNm() {
-		return careNm;
-	}
-	public void setCareNm(String careNm) {
-		this.careNm = careNm;
-	}
-	public String getCareTel() {
-		return careTel;
-	}
-	public void setCareTel(String careTel) {
-		this.careTel = careTel;
-	}
-	public String getCareAddr() {
-		return careAddr;
-	}
-	public void setCareAddr(String careAddr) {
-		this.careAddr = careAddr;
-	}
-	public String getOrgNm() {
-		return orgNm;
-	}
-	public void setOrgNm(String orgNm) {
-		this.orgNm = orgNm;
-	}
-	public String getChargeNm() {
-		return chargeNm;
-	}
-	public void setChargeNm(String chargeNm) {
-		this.chargeNm = chargeNm;
-	}
-	public String getOfficetel() {
-		return officetel;
-	}
-	public void setOfficetel(String officetel) {
-		this.officetel = officetel;
-	}
+
+    
 	@Override
 	public String toString() {
-		return "APIDto [desertionNo=" + desertionNo + ", filename=" + filename + ", happenDt=" + happenDt
-				+ ", happenPlace=" + happenPlace + ", kindCd=" + kindCd + ", colorCd=" + colorCd + ", age=" + age
-				+ ", weight=" + weight + ", noticeNo=" + noticeNo + ", noticeSdt=" + noticeSdt + ", noticeEdt="
+		return "DatasetDto [no=" + no + ", desertionNo=" + desertionNo + ", filename=" + filename + ", happenDt="
+				+ happenDt + ", happenPlace=" + happenPlace + ", kindCd=" + kindCd + ", colorCd=" + colorCd + ", age="
+				+ age + ", weight=" + weight + ", noticeNo=" + noticeNo + ", noticeSdt=" + noticeSdt + ", noticeEdt="
 				+ noticeEdt + ", popfile=" + popfile + ", processState=" + processState + ", sexCd=" + sexCd
 				+ ", neuterYn=" + neuterYn + ", specialMark=" + specialMark + ", careNm=" + careNm + ", careTel="
 				+ careTel + ", careAddr=" + careAddr + ", orgNm=" + orgNm + ", chargeNm=" + chargeNm + ", officetel="
 				+ officetel + "]";
 	}
-	public DatasetDto(String desertionNo, String filename, String happenDt, String happenPlace, String kindCd,
+
+
+	public DatasetDto(int no, String desertionNo, String filename, String happenDt, String happenPlace, String kindCd,
 			String colorCd, String age, String weight, String noticeNo, String noticeSdt, String noticeEdt,
 			String popfile, String processState, String sexCd, String neuterYn, String specialMark, String careNm,
 			String careTel, String careAddr, String orgNm, String chargeNm, String officetel) {
 		super();
+		this.no = no;
 		this.desertionNo = desertionNo;
 		this.filename = filename;
 		this.happenDt = happenDt;
@@ -193,5 +67,237 @@ public class DatasetDto {
 		this.chargeNm = chargeNm;
 		this.officetel = officetel;
 	}
+
+
+	public int getNo() {
+		return no;
+	}
+
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+
+	public String getDesertionNo() {
+		return desertionNo;
+	}
+
+
+	public void setDesertionNo(String desertionNo) {
+		this.desertionNo = desertionNo;
+	}
+
+
+	public String getFilename() {
+		return filename;
+	}
+
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+
+	public String getHappenDt() {
+		return happenDt;
+	}
+
+
+	public void setHappenDt(String happenDt) {
+		this.happenDt = happenDt;
+	}
+
+
+	public String getHappenPlace() {
+		return happenPlace;
+	}
+
+
+	public void setHappenPlace(String happenPlace) {
+		this.happenPlace = happenPlace;
+	}
+
+
+	public String getKindCd() {
+		return kindCd;
+	}
+
+
+	public void setKindCd(String kindCd) {
+		this.kindCd = kindCd;
+	}
+
+
+	public String getColorCd() {
+		return colorCd;
+	}
+
+
+	public void setColorCd(String colorCd) {
+		this.colorCd = colorCd;
+	}
+
+
+	public String getAge() {
+		return age;
+	}
+
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+
+	public String getWeight() {
+		return weight;
+	}
+
+
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+
+
+	public String getNoticeNo() {
+		return noticeNo;
+	}
+
+
+	public void setNoticeNo(String noticeNo) {
+		this.noticeNo = noticeNo;
+	}
+
+
+	public String getNoticeSdt() {
+		return noticeSdt;
+	}
+
+
+	public void setNoticeSdt(String noticeSdt) {
+		this.noticeSdt = noticeSdt;
+	}
+
+
+	public String getNoticeEdt() {
+		return noticeEdt;
+	}
+
+
+	public void setNoticeEdt(String noticeEdt) {
+		this.noticeEdt = noticeEdt;
+	}
+
+
+	public String getPopfile() {
+		return popfile;
+	}
+
+
+	public void setPopfile(String popfile) {
+		this.popfile = popfile;
+	}
+
+
+	public String getProcessState() {
+		return processState;
+	}
+
+
+	public void setProcessState(String processState) {
+		this.processState = processState;
+	}
+
+
+	public String getSexCd() {
+		return sexCd;
+	}
+
+
+	public void setSexCd(String sexCd) {
+		this.sexCd = sexCd;
+	}
+
+
+	public String getNeuterYn() {
+		return neuterYn;
+	}
+
+
+	public void setNeuterYn(String neuterYn) {
+		this.neuterYn = neuterYn;
+	}
+
+
+	public String getSpecialMark() {
+		return specialMark;
+	}
+
+
+	public void setSpecialMark(String specialMark) {
+		this.specialMark = specialMark;
+	}
+
+
+	public String getCareNm() {
+		return careNm;
+	}
+
+
+	public void setCareNm(String careNm) {
+		this.careNm = careNm;
+	}
+
+
+	public String getCareTel() {
+		return careTel;
+	}
+
+
+	public void setCareTel(String careTel) {
+		this.careTel = careTel;
+	}
+
+
+	public String getCareAddr() {
+		return careAddr;
+	}
+
+
+	public void setCareAddr(String careAddr) {
+		this.careAddr = careAddr;
+	}
+
+
+	public String getOrgNm() {
+		return orgNm;
+	}
+
+
+	public void setOrgNm(String orgNm) {
+		this.orgNm = orgNm;
+	}
+
+
+	public String getChargeNm() {
+		return chargeNm;
+	}
+
+
+	public void setChargeNm(String chargeNm) {
+		this.chargeNm = chargeNm;
+	}
+
+
+	public String getOfficetel() {
+		return officetel;
+	}
+
+
+	public void setOfficetel(String officetel) {
+		this.officetel = officetel;
+	}
+
+
 	public DatasetDto() {}
 }
