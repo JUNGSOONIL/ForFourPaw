dbd564828d13f1f3145303f963b4f754fbeab2bf
CSH <1525434@donga.ac.kr>
2022-03-27 19:41:58 +0900

    Fix S3Image
    Former-commit-id: 9f2c0176d1fc9f3e9d35fbad8e10c617a24f89b3

--- backend/FFP/src/main/java/com/ssafy/FFP/Service/S3Service.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Service/S3Service.java
@@ -1,5 +1,6 @@
 package com.ssafy.FFP.Service;
 
+import com.ssafy.FFP.Dto.S3Dto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -9,4 +10,5 @@ public interface S3Service {
     public List<Integer> uploadFile(List<MultipartFile> multipartFile);
     public String uploadFileReturnURL(int no, MultipartFile multipartFile);
     public void deleteFile(String fileName);
+    public int imgDBMS(S3Dto s3Dto);
 }
