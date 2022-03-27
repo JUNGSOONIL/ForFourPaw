42d3c99810a27935c4cea680c99248a223c27dc4
AlwaysRYU <aliverise@naver.com>
2022-03-27 16:29:29 +0900

    Add Region, Kind, Date Analysis

--- backend/FFP/src/main/java/com/ssafy/FFP/Dto/AnalysisResultDto.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Dto/AnalysisResultDto.java
@@ -22,6 +22,38 @@ public class AnalysisResultDto {
     public List<AnalysisDto> getList() { return list; }
     public void setList(List<AnalysisDto> list) { this.list = list; }
 
+    // 날짜 통계
+    private List<AnalysisDto> datelist;
+    public List<AnalysisDto> getDatelist() {
+        return datelist;
+    }
+    public void setDatelist(List<AnalysisDto> datelist) {
+        this.datelist = datelist;
+    }
+
+    // 동물별 통계
+    private List<AnalysisDto> kindlist;
+    public List<AnalysisDto> getKindlist() {
+        return kindlist;
+    }
+    public void setKindlist(List<AnalysisDto> kindlist) {
+        this.kindlist = kindlist;
+    }
+
+    // 품종별 통계
+    private List<AnalysisDto> kindlist2;
+    public List<AnalysisDto> getKindlist2() {
+        return kindlist2;
+    }
+    public void setKindlist2(List<AnalysisDto> list) {
+        this.kindlist2 = list;
+    }
+
+    // 지역별 통계
+    private List<AnalysisDto> regionlist;
+    public List<AnalysisDto> getRegionlist() { return regionlist; }
+    public void setRegionlist(List<AnalysisDto> regionlist) { this.regionlist = regionlist; }
+
     @Override
     public String toString() {
         return "AnalysisResultDto{" +
