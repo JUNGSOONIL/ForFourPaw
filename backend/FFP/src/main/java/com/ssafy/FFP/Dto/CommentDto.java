c751968b897bab31f004c2b21be31f563987a7a8
CSH <1525434@donga.ac.kr>
2022-03-31 14:14:23 +0900

    Fix  typo
    Former-commit-id: 182e0baa90685ad6b8b6ba47b723062e5cca75cb

--- backend/FFP/src/main/java/com/ssafy/FFP/Dto/CommentDto.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Dto/CommentDto.java
@@ -10,7 +10,7 @@ public class CommentDto {
     private String descr;
     private int missNo;
     private String authorNickName;
-    private String profileImage
+    private String profileImage;
 
     public CommentDto() {
     }
