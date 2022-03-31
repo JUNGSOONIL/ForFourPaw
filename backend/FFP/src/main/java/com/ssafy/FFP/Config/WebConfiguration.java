0a8348864027ebbee53d96818c4f00da8bfe6ff3
soonil <soonil0119@naver.com>
2022-03-31 19:22:22 +0900

    Update Interceptors in WebConfiguration.java

--- backend/FFP/src/main/java/com/ssafy/FFP/Config/WebConfiguration.java
+++ backend/FFP/src/main/java/com/ssafy/FFP/Config/WebConfiguration.java
@@ -33,8 +33,8 @@ public class WebConfiguration implements WebMvcConfigurer {
                 .excludePathPatterns("/kapi.kakao.com/**")
                 .excludePathPatterns("/error/**")
                 .excludePathPatterns("/alarm/**")
-                .excludePathPatterns("/register/**");
-
+                .excludePathPatterns("/register/**")
+                .excludePathPatterns("/dataset/**");
     }
   
 }
