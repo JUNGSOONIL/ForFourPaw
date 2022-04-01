68837ca056a7e26acf147f801e77e5df89a0ff3a
AlwaysRYU <aliverise@naver.com>
2022-04-01 17:13:37 +0900

    Fix Axios Timeout
    Former-commit-id: 4e45224f2cb77a1310e006a6f2e03db7b0acd910

--- frontend/src/views/analysis/AnalysisView.vue
+++ frontend/src/views/analysis/AnalysisView.vue
@@ -426,7 +426,7 @@ export default  {
                     url: '/api/test',
                     data: data, 
                     headers: headers,  // 넣는거 까먹지 마세요
-                    timeout : 180000,
+                    timeout : 1800000,
                 }).then((res) => {
 
                     this.$store.dispatch('login/accessTokenRefresh', res) // store아닌곳에서
