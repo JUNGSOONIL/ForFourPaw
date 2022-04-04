ee52505379b0ab52ffac1b90a64d418da778989b
AlwaysRYU <aliverise@naver.com>
2022-04-04 16:53:01 +0900

    Add AnimalkindChart and Update LoadDataMethod
    Former-commit-id: 550049effc72f3af15f4f9e6ce742c3d0e9d9ed1

--- frontend/src/views/analysis/AnalysisView.vue
+++ frontend/src/views/analysis/AnalysisView.vue
@@ -34,6 +34,7 @@
         <div class="row justify-content-center"> 
           <div id="t" class="counter-title text-center mb-65">
             <h6 class="sub-title">유기 동물 데이터 분석</h6>
+
             <h2 style="font-family:GW" id="t" >
               For Four Paw의 데이터 분석 서비스는
             </h2>
@@ -41,11 +42,11 @@
             <h1>총 1,174,858 건의 유기 동물 정보를 분석해서 </h1>
             <h2> 사용자에게 그 결과를 제공합니다. </h2>
           
-            <h5> 범위 : 2009.01 ~ 2022.03  </h5>
+            <h5> 범위 : 2009.01 ~ 현재  </h5>
 
-            <img id="analysisdog" @click="loaddata" src="../../assets/img/analysisDog4.png" style="cursor:pointer;" >
+            <img id="analysisdog" @click="loaddata_fromDB" src="../../assets/img/analysisDog4.png" style="cursor:pointer;" >
 
-            <h5>분석에 약 1분 소요됩니다.</h5>
+            <h5> 데이터는 매일 새벽 4시에 업데이트 됩니다. ( 한국시간기준 ) </h5>
           
           </div>
 
@@ -78,6 +79,19 @@
                   
                   
                 </div>
+
+                <div class="row justify-content-center">
+                  <div><h1> 유기된 동물의 종류  </h1></div>
+                  <div>
+                  </div>
+                </div>
+                
+                <div class="row justify-content-center">
+                  <div class="col" style="background-color:white">
+                    <canvas id="Chart_animal" style="width:100% height:100%"></canvas>
+                  </div>
+                </div> 
+
 
                 <div class="row justify-content-center">
                   <div><h1> 무게 별 유기동물 등록 건수  </h1></div>
@@ -120,8 +134,15 @@
 
 
                 <div class="row justify-content-center">
-                  <div><h1> 품종 별 WORD CLOUD </h1></div>
+                  <div id="t" class="counter-title text-center mb-65">
+                    <div><h1> 품종 별 WORD CLOUD </h1></div>
+                     <h5> ※ 코숏 : ( 코리안 숏헤어를 뜻하며, 품종이 정해지지 않은 한국의 혼혈 고양이)</h5>
+                     <h5> ※ 유기된 수가 120,000이 넘는 품종은 크기가 고정입니다. </h5>
+                     
+                  </div>
+
                   <div>
+                 
                   </div>
                 </div>
                 
@@ -130,6 +151,7 @@
                   <div class="col" style="background-color:white width:100% height:100%">
                     <div id="kind_wordcloud"></div>
                   </div>
+                  
                   </div>
                 </div> 
 
@@ -356,6 +378,7 @@ export default  {
             Chart_Date : null,
             Chart_year : null,
             Chart_region : null,
+            Chart_animal : null,
             words : [],
             isLoading : false,
             region_name : "지역이름",
@@ -411,7 +434,7 @@ export default  {
           this.region_name = this.region_result[index].key;
           this.region_value = this.region_result[index].value;
         }, 
-        loaddata() {
+        loaddata_fromDB() {
             console.log("[system] 차트 불러오기 ");
             
             let headers = {
@@ -424,7 +447,7 @@ export default  {
               type: ''
             };
               axios({
-                    method: 'get',
+                    method:'get',
                     url: '/api/test',
                     data: data, 
                     headers: headers,  // 넣는거 까먹지 마세요
@@ -520,7 +543,7 @@ export default  {
                     });
                     const ctx2 = document.getElementById('Chart_date').getContext('2d');
                     this.Chart_Date = new Chart( ctx2, {
-                        type: 'doughnut',
+                        type: 'bar',
                         data: {
                             labels : templaabels ,
                             datasets: [{
@@ -550,6 +573,47 @@ export default  {
                             plugins: {
                                 legend: {
                                   display : false
+                                }
+                            }, 
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+
+                    // 축종 별
+                    templaabels = [];
+                    tempdata = [];
+                    templaabels.push("강아지");
+                    templaabels.push("고양이");
+                    templaabels.push("기타 축종");
+                    res.data.kindlist.forEach(element => {
+                        tempdata.push(element.value);
+                    });
+                    const ctx4 = document.getElementById('Chart_animal').getContext('2d');
+                    this.Chart_animal = new Chart( ctx4, {
+                        type: 'pie',
+                        data: {
+                            labels : templaabels ,
+                            datasets: [{
+                                label: '# of Votes',
+                                data : tempdata,
+                                borderWidth: 1,
+                                fill : "start" ,
+                                backgroundColor: [
+                                  '#095000',
+                                  '#f1fba0',
+                                  '#622c1c',
+                                ],
+
+                            }]
+                        },
+                        options: {
+                            plugins: {
+                                legend: {
+                                  display : true
                                 }
                             }, 
                             scales: {
@@ -736,7 +800,6 @@ export default  {
             document.getElementById('Sejong').style.fill = "#5e0041";
           }
         },
-
 
     }
 
