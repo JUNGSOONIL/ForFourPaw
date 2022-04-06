8cad610335941c32077674e420233f895369f378
윤찬호 <cmsj72@naver.com>
2022-04-06 17:48:49 +0900

    Fix footerFE
    Former-commit-id: 7a29ed6fc27774e7348ef833f72e485cf55d78b7

--- frontend/src/views/FooterBar.vue
+++ frontend/src/views/FooterBar.vue
@@ -8,12 +8,7 @@
             <div class="col-3">
               <div class="footer-widget">
                 <div class="f-logo">
-                  <router-link to="/"
-                    ><img
-                      src="/img/logo/paw_logo_2line.png"
-                      alt=""
-                      onclick="window.scrollTo(0,0);"
-                  /></router-link>
+                  <img src="/img/logo/paw_logo_2line.png" alt="" />
                 </div>
                 <div class="footer-text">
                   <p>
@@ -21,136 +16,71 @@
                     조회할 수 있습니다.
                   </p>
                 </div>
-                <!-- <div class="footer-contact">
-                  <div class="icon"><i class="fas fa-headphones"></i></div>
-                  <div class="content">
-                    <h4 class="title">
-                      <a href="tel:0987654321">747-800-9880</a>
-                    </h4>
-                    <span>Call Now</span>
-                  </div>
-                </div>
-                <div class="footer-social">
-                  <ul>
-                    <li>
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-youtube"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                    </li>
-                  </ul>
-                </div> -->
               </div>
             </div>
-            <!-- <div class="col-2">
-              <div class="footer-widget">
-                <h4 class="fw-title">Our Policies</h4>
-                <div class="fw-link">
-                  <ul>
-                    <li><a href="contact.html">Privacy Policy</a></li>
-                    <li><a href="contact.html">Terms and Conditions</a></li>
-                    <li><a href="contact.html">Editorial Policy</a></li>
-                    <li><a href="contact.html">Return Policy</a></li>
-                    <li><a href="contact.html">IP Policy</a></li>
-                    <li>
-                      <a href="contact.html">Grievance Redressal Policy</a>
-                    </li>
-                    <li><a href="contact.html">Our Conditions</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div class="col-2">
-              <div class="footer-widget">
-                <h4 class="fw-title">Our Services</h4>
-                <div class="fw-link">
-                  <ul>
-                    <li><a href="breeder.html">Our Breeder</a></li>
-                    <li><a href="adoption.html">Our Adoption</a></li>
-                    <li><a href="contact.html">Editorial Policy</a></li>
-                    <li><a href="contact.html">Return Policy</a></li>
-                    <li><a href="contact.html">Grievance Policy</a></li>
-                    <li><a href="contact.html">Redressal Policy</a></li>
-                    <li><a href="contact.html">Contact Us</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div> -->
             <div class="col-7">
               <div class="footer-widget">
                 <h4 class="fw-title">Team</h4>
                 <div class="fw-insta">
                   <ul>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/team/soon.png"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/team/soon.png"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                       정순일
                     </li>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/team/sik.jpg"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/team/sik.jpg"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                       우윤식
                     </li>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/team/tak.jpg"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/team/tak.jpg"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                       류기탁
                     </li>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/team/sang.png"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/team/sang.png"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                       최상후
                     </li>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/team/chan.png"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/team/chan.png"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                       윤찬호
                     </li>
                     <li>
-                      <a href="#"
-                        ><img
-                          class="team"
-                          src="/img/logo/character.png"
-                          alt=""
-                          width="92px"
-                          height="92px"
-                      /></a>
+                      <img
+                        class="team"
+                        src="/img/logo/character.png"
+                        alt=""
+                        width="92px"
+                        height="92px"
+                      />
                     </li>
                   </ul>
                 </div>
@@ -173,36 +103,6 @@
                 <p>Copyright &copy; 2021 Petco. All Rights Reserved.</p>
               </div>
             </div>
-            <!-- <div class="col-md-4 d-none d-md-block">
-              <div class="footer-lang">
-                <div class="dropdown">
-                  <button
-                    class="dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton2"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    <img src="/img/icon/united-states.png" alt="" /> English
-                  </button>
-                  <div
-                    class="dropdown-menu"
-                    aria-labelledby="dropdownMenuButton2"
-                  >
-                    <a class="dropdown-item" href="index.html"
-                      ><img src="/img/icon/russia.png" alt="" />Russia</a
-                    >
-                    <a class="dropdown-item" href="index.html"
-                      ><img src="/img/icon//thailand.png" alt="" />Thailand</a
-                    >
-                    <a class="dropdown-item" href="index.html"
-                      ><img src="/img/icon/india.png" alt="" />India</a
-                    >
-                  </div>
-                </div>
-              </div>
-            </div> -->
           </div>
         </div>
       </div>
